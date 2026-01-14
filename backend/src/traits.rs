d2f0ce64f745343842859105536efc3fefb85aaa
m5l14i11 <siarhei.melnik@proton.me>
2026-01-14 13:33:10 +0300

    add kernels

--- backend/src/traits.rs
+++ backend/src/traits.rs
@@ -8,14 +8,26 @@ pub trait Storage<F>: Debug + Clone + Send + Sync {
     fn as_mut_slice(&mut self) -> &mut [F];
 }
 
-pub trait Evaluator<F: Real, B: Backend<F> + ?Sized> {
-    fn eval(&self, backend: &mut B) -> B::Repr;
+pub trait UnaryKernel<S> {
+    fn apply(inp: &S, out: &mut S);
+}
+
+pub trait BinaryKernel<S> {
+    fn apply(lhs: &S, rhs: &S, out: &mut S);
 }
 
 pub trait Backend<F: Real> {
     type Repr: Storage<F>;
 
     fn pure(&mut self, data: &[F]) -> Self::Repr;
+
+    fn binary<K>(&mut self, lhs: &Self::Repr, rhs: &Self::Repr) -> Self::Repr
+    where
+        K: BinaryKernel<Self::Repr>;
+
+    fn unary<K>(&mut self, inp: &Self::Repr) -> Self::Repr
+    where
+        K: UnaryKernel<Self::Repr>;
 
     fn compute<E>(&mut self, expr: &E) -> Self::Repr
     where
@@ -24,4 +36,8 @@ pub trait Backend<F: Real> {
     {
         expr.eval(self)
     }
+}
+
+pub trait Evaluator<F: Real, B: Backend<F> + ?Sized> {
+    fn eval(&self, backend: &mut B) -> B::Repr;
 }
