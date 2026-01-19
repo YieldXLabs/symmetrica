01200942a7c3a110e1cc336578bc6d1cf31e677b
m5l14i11 <siarhei.melnik@proton.me>
2026-01-19 21:53:34 +0300

    into sub

--- tensor/src/eval.rs
+++ tensor/src/eval.rs
@@ -1,6 +1,6 @@
 use super::{Base, Evaluator};
-use algebra::{AddExpr, Real, ScaleExpr};
-use backend::{AddKernel, Backend};
+use algebra::{AddExpr, Real, ScaleExpr, SubExpr};
+use backend::{AddKernel, Backend, SubKernel};
 
 impl<F, B, L, Rhs, const R: usize> Evaluator<F, B, R> for AddExpr<L, Rhs>
 where
@@ -14,6 +14,23 @@ where
         let r = self.right.eval(backend);
 
         let storage = backend.binary::<AddKernel>(&l.storage, &r.storage);
+
+        Base::new(storage, l.shape)
+    }
+}
+
+impl<F, B, L, Rhs, const R: usize> Evaluator<F, B, R> for SubExpr<L, Rhs>
+where
+    F: Real,
+    B: Backend<F>,
+    L: Evaluator<F, B, R>,
+    Rhs: Evaluator<F, B, R>,
+{
+    fn eval(&self, backend: &mut B) -> Base<B::Repr, F, R> {
+        let l = self.left.eval(backend);
+        let r = self.right.eval(backend);
+
+        let storage = backend.binary::<SubKernel>(&l.storage, &r.storage);
 
         Base::new(storage, l.shape)
     }
