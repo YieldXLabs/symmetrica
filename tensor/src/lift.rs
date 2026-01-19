01847d786c5340ed9df9223e7b138d2e65824a64
m5l14i11 <siarhei.melnik@proton.me>
2026-01-19 21:21:45 +0300

    add lifted

--- tensor/src/lift.rs
+++ tensor/src/lift.rs
@@ -1,6 +1,24 @@
-use algebra::Real;
+use super::Tensor;
+use algebra::{ConstExpr, Real, Shape};
+
 pub trait Lift<F: Real> {
     type Output;
 
     fn lift(self) -> Self::Output;
+}
+
+impl<F: Real> Lift<F> for F {
+    type Output = ConstExpr<F>;
+
+    fn lift(self) -> Self::Output {
+        ConstExpr(self)
+    }
+}
+
+impl<F: Real, Sh: Shape, const R: usize, E> Lift<F> for Tensor<F, Sh, R, E> {
+    type Output = E;
+
+    fn lift(self) -> Self::Output {
+        self.expr
+    }
 }
--- tensor/src/ops.rs
+++ tensor/src/ops.rs
@@ -1,19 +1,20 @@
-use super::Tensor;
+use super::{Lift, Tensor};
 use algebra::{AddExpr, Real, Shape};
 use std::{marker::PhantomData, ops::Add};
 
-impl<F, Sh, const R: usize, L, Rhs> Add<Tensor<F, Sh, R, Rhs>> for Tensor<F, Sh, R, L>
+impl<F, Sh, const R: usize, L, Rhs> Add<Rhs> for Tensor<F, Sh, R, L>
 where
     F: Real,
     Sh: Shape,
+    Rhs: Lift<F>,
 {
-    type Output = Tensor<F, Sh, R, AddExpr<L, Rhs>>;
+    type Output = Tensor<F, Sh, R, AddExpr<L, Rhs::Output>>;
 
-    fn add(self, rhs: Tensor<F, Sh, R, Rhs>) -> Self::Output {
+    fn add(self, rhs: Rhs) -> Self::Output {
         Tensor {
             expr: AddExpr {
                 left: self.expr,
-                right: rhs.expr,
+                right: rhs.lift(),
             },
             _marker: PhantomData,
         }
