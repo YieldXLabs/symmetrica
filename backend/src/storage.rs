45ca374d95b82b5fe9092353c9164c7912888148
m5l14i11 <siarhei.melnik@proton.me>
2026-02-02 12:49:56 +0300

    upd storage cheap clone

--- backend/src/storage.rs
+++ backend/src/storage.rs
@@ -2,31 +2,25 @@ use super::Storage;
 use algebra::Data;
 use std::alloc::{Layout, alloc_zeroed, dealloc};
 use std::ptr::NonNull;
+use std::sync::Arc;
 
 #[derive(Debug)]
-pub struct UnifiedStorage<F: Data> {
-    pub ptr: NonNull<F>,
-    pub len: usize,
+struct RawBuffer<F: Data> {
+    ptr: NonNull<F>,
+    len: usize,
 }
 
-unsafe impl<F: Data> Send for UnifiedStorage<F> {}
-unsafe impl<F: Data> Sync for UnifiedStorage<F> {}
+unsafe impl<F: Data> Send for RawBuffer<F> {}
+unsafe impl<F: Data> Sync for RawBuffer<F> {}
 
-impl<F: Data> Storage for UnifiedStorage<F> {
-    type Elem = F;
-
-    fn len(&self) -> usize {
-        self.len
-    }
-
-    fn alloc(n: usize) -> Self {
+impl<F: Data> RawBuffer<F> {
+    fn new(n: usize) -> Self {
         if n == 0 {
             return Self {
                 ptr: NonNull::dangling(),
                 len: 0,
             };
         }
-
         let layout = Layout::array::<F>(n).expect("Allocation too large");
         unsafe {
             let raw = alloc_zeroed(layout) as *mut F;
@@ -44,25 +38,20 @@ impl<F: Data> Storage for UnifiedStorage<F> {
     }
 }
 
-impl<F: Data> Clone for UnifiedStorage<F> {
+impl<F: Data> Clone for RawBuffer<F> {
     fn clone(&self) -> Self {
-        let new_storage = <Self as Storage>::alloc(self.len);
+        let new_buf = Self::new(self.len);
 
         if self.len > 0 {
             unsafe {
-                std::ptr::copy_nonoverlapping(
-                    self.ptr.as_ptr(),
-                    new_storage.ptr.as_ptr(),
-                    self.len,
-                );
+                std::ptr::copy_nonoverlapping(self.ptr.as_ptr(), new_buf.ptr.as_ptr(), self.len);
             }
         }
-
-        new_storage
+        new_buf
     }
 }
 
-impl<F: Data> Drop for UnifiedStorage<F> {
+impl<F: Data> Drop for RawBuffer<F> {
     fn drop(&mut self) {
         if self.len > 0 {
             let layout = Layout::array::<F>(self.len).unwrap();
@@ -70,5 +59,35 @@ impl<F: Data> Drop for UnifiedStorage<F> {
                 dealloc(self.ptr.as_ptr() as *mut u8, layout);
             }
         }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct UnifiedStorage<F: Data> {
+    inner: Arc<RawBuffer<F>>,
+}
+
+unsafe impl<F: Data> Send for UnifiedStorage<F> {}
+unsafe impl<F: Data> Sync for UnifiedStorage<F> {}
+
+impl<F: Data> Storage for UnifiedStorage<F> {
+    type Elem = F;
+
+    fn len(&self) -> usize {
+        self.inner.len
+    }
+
+    fn alloc(n: usize) -> Self {
+        Self {
+            inner: Arc::new(RawBuffer::new(n)),
+        }
+    }
+
+    fn as_slice(&self) -> &[F] {
+        self.inner.as_slice()
+    }
+
+    fn as_mut_slice(&mut self) -> &mut [F] {
+        Arc::make_mut(&mut self.inner).as_mut_slice()
     }
 }
