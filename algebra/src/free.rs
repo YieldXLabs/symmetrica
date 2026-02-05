15bdd45f4644739d28e06ffcbd66998061ba2780
m5l14i11 <siarhei.melnik@proton.me>
2026-02-05 18:36:17 +0300

    todo

--- algebra/src/free.rs
+++ algebra/src/free.rs
@@ -40,6 +40,9 @@ pub struct ReshapeExpr<Op, const R_IN: usize, const R_OUT: usize> {
     pub new_shape: [usize; R_OUT],
 }
 
+// TODO: SliceExpr.
+// `Select` reduces rank (getting a single index).
+// We also need `Slice` to get a range (e.g., `tensor[0..10]`), which preserves rank.
 #[derive(Debug, Clone)]
 pub struct SelectExpr<Op> {
     pub op: Op,
@@ -67,6 +70,10 @@ pub struct ContractExpr<L, R> {
     pub axes: (Vec<usize>, Vec<usize>),
 }
 
+// TODO: Windowing / Convolution.
+// For trading, `RollingWindow` (e.g., Simple Moving Average) is distinct from Scan.
+// Scan is recursive (state depends on prev state). Window is parallelizable.
+// struct WindowExpr<Op, K> { op: Op, size: usize, kernel: K }
 #[derive(Debug, Clone)]
 pub struct ScanExpr<Op, F, K> {
     pub op: Op,
@@ -75,8 +82,12 @@ pub struct ScanExpr<Op, F, K> {
 }
 
 // TODO: Distance metrics
+// Implementation strategy:
+// 1. Define trait `Distance<F>` with `fn dist(a, b) -> F`.
+// 2. `Euclidean` = Sqrt(Sum(Square(Sub(a, b)))) -> This can be composed of existing kernels!
+// 3. `Cosine` = Div(Dot(a, b), Mul(Norm(a), Norm(b))).
+//
 // pub trait DistanceMetric<F: Real>: Copy + Clone + 'static {}
-
 // #[derive(Debug, Clone, Copy)]
 // pub struct Euclidean;
 // impl<F: Real> DistanceMetric<F> for Euclidean {}
@@ -88,7 +99,16 @@ pub struct ScanExpr<Op, F, K> {
 //     _metric: PhantomData<M>,
 // }
 
-// TODO: Match exprs
+// TODO: Rewrite Rules / Pattern Matching.
+// To implement optimizations (e.g., x + 0 -> x), you need a "Rewrite" trait.
+// Since Rust structs are static, you can't easily match `Add(_, Zero)` dynamically
+// unless you use the Enum wrapper approach mentioned at the top.
+//
+// Example infrastructure needed:
+// trait Rewriter {
+//     fn rewrite(&self, expr: &Expr) -> Option<Expr>;
+// }
+//
 // MatchExpr::new(|expr| matches!(expr, Add(_, Zero) => true));
 // MatchExpr::new(|expr| matches!(expr, Sub(x, x) => true));
 // MatchExpr::new(|expr| matches!(expr, Scale(Add(a,b), k) => Add(Scale(a,k), Scale(b,k))));
