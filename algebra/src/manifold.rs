b76dc03d6067b638d3c6982c70f6dcf8dcee2ac4
m5l14i11 <siarhei.melnik@proton.me>
2026-02-11 01:59:34 +0300

    todo

--- algebra/src/manifold.rs
+++ algebra/src/manifold.rs
@@ -15,3 +15,42 @@ pub trait Manifold<F: Real> {
     // Riemannian Inner Product
     fn inner(&self, p: &Self::Point, u: &Self::Tangent, v: &Self::Tangent) -> F;
 }
+
+// TODO: Dynamic stop
+// struct StopState<F> {
+//     distance: F,   // > 0
+//     rate: F,       // (0, r_max)
+// }
+
+// struct StopTangent<F> {
+//     d_dist: F,
+//     d_rate: F,
+// }
+
+// impl<F: Real> Manifold<F> for StopManifold {
+//     type Point = StopState<F>;
+//     type Tangent = StopTangent<F>;
+
+//     fn exp_map(&self, p: &Self::Point, v: &Self::Tangent) -> Self::Point {
+//         StopState {
+//             distance: p.distance * (v.d_dist).exp(),
+//             rate: clamp(p.rate + v.d_rate, F::zero(), self.r_max),
+//         }
+//     }
+
+//     fn project(&self, p: &Self::Point) -> Self::Point {
+//         StopState {
+//             distance: p.distance.max(self.min_dist),
+//             rate: clamp(p.rate, F::zero(), self.r_max),
+//         }
+//     }
+
+//     fn inner(&self, _p: &Self::Point, u: &Self::Tangent, v: &Self::Tangent) -> F {
+//         u.d_dist * v.d_dist + u.d_rate * v.d_rate
+//     }
+
+//     fn dist(&self, p: &Self::Point, q: &Self::Point) -> F {
+//         ((q.distance / p.distance).ln().powi(2)
+//          + (q.rate - p.rate).powi(2)).sqrt()
+//     }
+// }
